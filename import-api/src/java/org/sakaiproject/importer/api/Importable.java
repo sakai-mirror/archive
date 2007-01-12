7337dc5cefffee166c4b8d5d36e6e6075745cee4
zach.thomas@txstate.edu <zach.thomas@txstate.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-01-12 01:12:06 +0000

    Finally beginning to document my code.
    git-svn-id: https://source.sakaiproject.org/svn/archive/branches/import-using-entities@20284 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- import-api/src/java/org/sakaiproject/importer/api/Importable.java
+++ import-api/src/java/org/sakaiproject/importer/api/Importable.java
@@ -21,13 +21,48 @@
 
 package org.sakaiproject.importer.api;
 
+/**
+ * Defines the minimum attributes for an object that can be pushed into Sakai by one or more import handlers.
+ *
+ * @author <a href="mailto:zach.thomas@txstate.edu">Zach A. Thomas</a>
+ */
 public interface Importable {
+	/**
+	 * @return a globally unique identifier for this object
+	 */
 	String getGuid();
+	/**
+	 * @return a String identifying the semantic type of this object, e.g. "sakai-qti-assessment"
+	 */
 	String getTypeName();
+	/**
+	 * @return a label that identifies a logical grouping that this object occupied in its 
+	 *         previous home, e.g. "Course Documents" from a Blackboard system
+	 */
 	String getLegacyGroup();
+	/**
+	 * @return a path that can be used to place this object in the appropriate place in Sakai, 
+	 *         e.g. "/Chapter 2/review"
+	 */
 	String getContextPath();
+	/**
+	 * @return another Importable that can be considered the logical container of this object, 
+	 *         e.g. a question pool can be the parent of a question
+	 */
 	Importable getParent();
+	/**
+	 * @param another Importable that can be considered the logical container of this object, 
+	 *        e.g. a question pool can be the parent of a question
+	 */
 	void setParent(Importable parent);
+	/**
+	 * @param a label that identifies a logical grouping that this object occupied in its 
+	 *        previous home, e.g. "Course Documents" from a Blackboard system
+	 */
 	void setLegacyGroup(String legacyGroup);
+	/**
+	 * @param a path that can be used to place this object in the appropriate place in Sakai, 
+	 *        e.g. "/Chapter 2/review"
+	 */
 	void setContextPath(String path);
 }
